0e274a63cfff6b3ba559417f95bfed81f515097b
Darrell Martin <darrell@briarmedia.biz>
2026-04-12 04:04:31 +0100

    [ALL] Updated ALiVE version to 3.2.0.2604121

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -3,10 +3,10 @@
 
 // TODO: Consider Mod-wide or Component-narrow versions (or both, depending on wishes!)
 #define MAJOR 3
-#define MINOR 1
+#define MINOR 2
 #define PATCHLVL 0
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 2604114
+#define BUILD 2604121
 
 #define CLUSTERBUILD "Arma 3","Arma3",220,152984,"Stable"
 
